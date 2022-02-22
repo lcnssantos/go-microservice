ae191f64d8bcb838b74cd8d613b4af65bd35dd27
Luciano Santos <luciano.ssants@gmail.com>
2022-02-21 22:34:44 -0300

    Add users package

--- packages/users/User.go
+++ packages/users/User.go
@@ -1,1 +1,18 @@
 package users
+
+import "time"
+
+type User struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type CreateUser struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
